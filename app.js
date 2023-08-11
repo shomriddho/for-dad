e56ad3648661cb116f9a163c5ab1be983f4f3d93
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2023-08-11 23:01:01 +0600

    slide

--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-function myFunction(){let e=document.getElementById("myTopnav");"topnav"===e.className?e.className+=" responsive":e.className="topnav"}function showSlides(e){let l=e,s=document.getElementsByClassName("mySlides"),n=document.getElementsByClassName("dot");e>s.length&&(l=1),e<1&&(l=s.length);for(let t=0;t<s.length;t++)s[t].style.display="none";for(let o=0;o<n.length;o++)n[o].className=n[o].className.replace(" active","");s[l-1].style.display="block",n[l-1].className+=" active"}function plusSlides(e){showSlides(slideIndex+=e)}function currentSlide(e){showSlides(slideIndex=e)}function carousel(){let e=document.getElementsByClassName("mySlides");for(let l=0;l<e.length;l++)e[l].style.display="none";++slideIndex>e.length&&(slideIndex=1),e[slideIndex-1].style.display="block",setTimeout(carousel,12e3)}function scrollFunction(){let e=document.getElementById("myBtn");document.body.scrollTop>20||document.documentElement.scrollTop>20?e.style.display="block":e.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=scrollFunction;let slideIndex=1;showSlides(slideIndex),carousel();
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  let slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+  }
+
+  carousel();
+
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    
+    setTimeout(carousel, 12000); // Change image every 14 seconds
+  }
+  // Initialize and add the map
+  let mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
