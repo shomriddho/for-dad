089c29af1b3750410fa6ea39ffd2939e14ac3ce9
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2023-08-15 19:54:51 +0600

    seo

---
+++ Products/network-communication.js
@@ -0,0 +1,21 @@
+function openCity(evt, cityName) {
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    const tablinks = document.getElementsByClassName("tablinks");
+  
+    Array.from(tabcontent).forEach((tab) => {
+      tab.style.display = "none";
+    });
+  
+    Array.from(tablinks).forEach((link) => {
+      link.classList.remove("active");
+    });
+
+    document.getElementById(cityName).style.display = "block";
+  
+    if (evt && evt.currentTarget) {
+      evt.currentTarget.classList.add("active");
+    }
+  }
+  
+  openCity(null, "Overview");
+  
