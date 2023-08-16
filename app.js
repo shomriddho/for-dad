8f9ee916db49279d07dde61cec73d8514d2e1f4d
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2023-08-16 19:09:29 +0600

    bot

--- app.js
+++ app.js
@@ -72,7 +72,3 @@ function ifoff() {
   }
   
   // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
