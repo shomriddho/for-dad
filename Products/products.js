72e1d03819fe3050e98460202d7c982f7ac68efb
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2023-08-19 22:03:41 +0600

    as

--- Products/products.js
+++ Products/products.js
@@ -1,21 +1,21 @@
 function openCity(evt, cityName) {
-    const tabcontent = document.getElementsByClassName("tabcontent");
-    const tablinks = document.getElementsByClassName("tablinks");
-  
-    Array.from(tabcontent).forEach((tab) => {
-      tab.style.display = "none";
-    });
-  
-    Array.from(tablinks).forEach((link) => {
-      link.classList.remove("active");
-    });
+  const tabcontent = document.querySelectorAll(".tabcontent");
+  const tablinks = document.querySelectorAll(".tablinks");
 
-    document.getElementById(cityName).style.display = "block";
-  
-    if (evt && evt.currentTarget) {
-      evt.currentTarget.classList.add("active");
-    }
+  tabcontent.forEach((tab) => {
+    tab.style.display = "none";
+  });
+
+  tablinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+
+  document.getElementById(cityName).style.display = "block";
+
+  if (evt && evt.currentTarget) {
+    evt.currentTarget.classList.add("active");
   }
+}
   
   openCity(null, "Overview");
   
